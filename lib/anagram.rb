f6514c88e92ff52a2d3efc986135a0b00ae87bc7
victoria-huang <vctrah@gmail.com>
2018-03-18 14:12:34 -0400

    new class and methods

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    words.select do |match_word|
+      match_word.split("").sort == @word.split("").sort
+    end
+  end
+end
